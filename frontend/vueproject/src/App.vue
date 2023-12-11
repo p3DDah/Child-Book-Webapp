48b4a9a84b66d19f39f37ed2a11b350955ec8b4b
yjk54 <146088525+yjk54@users.noreply.github.com>
2023-12-11 17:21:07 +0900

    config

--- frontend/vueproject/src/App.vue
+++ frontend/vueproject/src/App.vue
@@ -1,30 +1,62 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class='container'>
+    <tests v-if='testing'></tests>
+
+    <div class='row'>
+      <div class='col'>
+        <nav-bar
+          :tests-open='testing'
+          @toggle-tests='toggleTests'>
+        </nav-bar>
+      </div>
+    </div>
+    <div class='row mt-2'>
+      <div class='col'>
+        <complaint-form ref='complaintForm'></complaint-form>
+      </div>
+    </div>  
+  </div>
 </template>
 
-<style lang="scss">
+<script>
+// import NavBar from './components/NavBar.vue';
+// import ComplaintForm from './components/ComplaintForm.vue';
+// import Tests from './components/Tests.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ComplaintForm,
+    NavBar,
+    Tests
+  },
+  data() {
+    return {
+      testing: false,
+    }
+  },
+  methods: {
+    toggleTests() {
+      this.testing = !this.testing;
+    }
+  }  
+}
+</script>
+
+<style>
+body { 
+  background: url('assets/bg.jpg') no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  -moz-osx-font-smoothing: grayscnale;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import'~bootstrap/dist/css/bootstrap.css'
 </style>
